9b8c6e4dc1528c7957e80d3f297076879361b6e0
Calvin <xcalboy11x@gmail.com>
2014-03-17 16:39:59 -0400

    Fixing User and SQLiteHelper Classes
    getters and setters for User class
    Started filling in SQLiteHelper class

--- src/com/techbearcave/AYDY/SQLiteHelper.java
+++ src/com/techbearcave/AYDY/SQLiteHelper.java
@@ -1,5 +1,56 @@
 package com.techbearcave.AYDY;
 
-public class SQLiteHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+public class SQLiteHelper extends SQLiteOpenHelper{
+	private static final String DATABASE_NAME = "areyoudoneyet.db";
+	private static final int SCHEMA_VERSION = 1;
+	
+	// Table Names
+    private static final String TABLE_USER = "users";
+    private static final String TABLE_TASK = "tasks";
+    private static final String TABLE_NOTE = "notes";
+    
+    // Common column names
+    private static final String KEY_CREATED_AT = "created_at";
+    private static final String KEY_ID = "id";
+    
+    // Note table create statement
+    private static final String CREATE_TABLE_USER = "CREATE TABLE "
+            + TABLE_USER + "(" + KEY_ID + " INTEGER PRIMARY KEY, " +
+            		"Fname TEXT, Lname TEXT, Password TEXT, Email TEXT" + ")";
+ 
+    // Tag table create statement
+    private static final String CREATE_TABLE_TASKS = "CREATE TABLE " + TABLE_TASK
+            + "(" + KEY_ID + " INTEGER PRIMARY KEY, Task TEXT, " 
+    		+ KEY_CREATED_AT + " DATETIME, FOREIGN KEY (" + TABLE_USER + ") REFERENCES "
+            + TABLE_USER + "(" + KEY_ID + "))";
+ 
+    // todo_tag table create statement
+    private static final String CREATE_TABLE_NOTE = "CREATE TABLE "
+            + TABLE_NOTE + "(" + KEY_ID + " INTEGER PRIMARY KEY, Description TEXT, "
+            + KEY_CREATED_AT + " DATETIME, FOREIGN KEY (" + TABLE_USER + ") REFERENCES "
+            + TABLE_USER + "(" + KEY_ID + "))";
+	
+	public SQLiteHelper(Context context) {
+		super(context, DATABASE_NAME, null, SCHEMA_VERSION);
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL("CREATE TABLE restaurants (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, type TEXT, notes TEXT);");
+	}
+	
+	@Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_TASK);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NOTE);
+ 
+        // create new tables
+        onCreate(db);
+    }
 }
--- src/com/techbearcave/AYDY/User.java
+++ src/com/techbearcave/AYDY/User.java
@@ -1,10 +1,44 @@
 package com.techbearcave.AYDY;
 
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class User  {
 
+	private String userId;
+	private String userPassword;
+	private String userEmail;
 
+	public User (String id, String password, String email) {
+		this.userId = id;
+		this.userPassword = password;
+		this.userEmail = email;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(String id) {
+		this.userId = id;
+	}
+	
+	public String getUserPassword() {
+		return userPassword;
+	}
+	
+	public void setUserPassword(String password) {
+		this.userPassword = password;
+	}
+	
+	public String getUserEmail() {
+		return userEmail;
+	}
+	
+	public void setUserEmail(String email) {
+		this.userEmail = email;
+	}
 }

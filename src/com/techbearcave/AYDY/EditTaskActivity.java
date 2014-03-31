4be3c41fd8d20ff17d426d47e8e37024df2e6e11
Calvin <xcalboy11x@gmail.com>
2014-03-31 15:11:26 -0400

    starting on task

--- src/com/techbearcave/AYDY/EditTaskActivity.java
+++ src/com/techbearcave/AYDY/EditTaskActivity.java
@@ -1,24 +1,154 @@
 package com.techbearcave.AYDY;
 
+import java.util.Calendar;
+
 import com.techbearcave.notetaker.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditTaskActivity extends Activity {
+	
+	public static final int RESULT_DELETE = -9000;
+	private boolean isInEditMode = true ; 
+	private boolean isAddingNote = true; 
+	private String userId;
+	private String taskId;
+	private Button saveButton;
+	private Button cancelButton;
+	private EditText titleEditText;
+	private EditText taskEditText;
+	private SQLiteHelper helper;
+	private TextView dateTextView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_task);
+		
+		saveButton = (Button)findViewById(R.id.saveButton);
+        cancelButton = (Button)findViewById(R.id.cancelButton);
+    	titleEditText = (EditText)findViewById(R.id.titleEditText);
+		taskEditText = (EditText)findViewById(R.id.taskEditText);
+		dateTextView = (TextView)findViewById(R.id.dateTextView);
+		helper = new SQLiteHelper(this);
+		Bundle bundle = this.getIntent().getExtras();
+		userId = (String) bundle.getSerializable ("stringToPassOn");
+		isInEditMode = getIntent().getBooleanExtra(ListNotesActivity.isInEditMode, true);
+		
+		System.out.println("userID: "+ userId);
+		
+		
+		if (isInEditMode)
+		{
+			taskId = (String) bundle.getSerializable ("positionTracker");
+			System.out.println("noteID: "+ taskId);
+			load();
+		}
+		// cancel method to cancel a new note
+		cancelButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+				
+			}
+		});
+		
+		// save method to save a new note
+		
+        saveButton.setOnClickListener(new OnClickListener() {
+			// when the save button is clicked we want to check to see if were "editing" the note
+        	// or if it is a brand new note 
+        	
+			@Override
+			public void onClick(View v) {
+				System.out.println("Inside click");
+				if(!isInEditMode)
+				{
+					System.out.println("Add mode");
+					helper.insertNote(titleEditText.getText().toString(), taskEditText.getText().toString(), 
+									Calendar.getInstance().getTime().toString(), Integer.parseInt(userId));	
+					finish();
+				}
+				else
+				{
+					// update method
+					System.out.println("Edit mode");
+					helper.updateNote(titleEditText.getText().toString(), taskEditText.getText().toString(), Integer.parseInt(taskId));
+					finish();
+				}
+
+			}
+		});
 	}
+	
+	 @Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			
+	    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	    	builder.setMessage(R.string.confirmMsg);
+	    	builder.setTitle("Confirm Delete");
+	    	builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {				
+					
+					
+					
+					finish();
+				}
+			} );
+	    	
+	    	builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+				
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					
+					
+				}
+			});
+	    	
+	    	builder.create().show();
+	    	return true;
+		}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.edit_task, menu);
-		return true;
+		getMenuInflater().inflate(R.menu.delete_note_from_menu, menu);
+        if(isAddingNote)
+        {
+        	menu.removeItem(R.id.deleteNote);
+        }
+        return true;
 	}
 
+	private void load() {
+    	Bundle bundle = this.getIntent().getExtras();
+		taskId = (String) bundle.getSerializable("positionTracker");
+		Cursor c = helper.getNoteByNoteId(taskId, userId);
+		
+
+		System.out.println("Edit mode");
+		
+
+		c.moveToFirst();
+		
+		titleEditText.setText(helper.getNotename(c));
+		taskEditText.setText(helper.getNotedescription(c));
+		dateTextView.setText(helper.getCreatedat(c));
+		
+		c.close();
+	}
 }
--- src/com/techbearcave/AYDY/ListTasksActivity.java
+++ src/com/techbearcave/AYDY/ListTasksActivity.java
@@ -1,17 +1,101 @@
 package com.techbearcave.AYDY;
 
+import com.techbearcave.AYDY.ListNotesActivity.NoteAdapter;
+import com.techbearcave.AYDY.ListNotesActivity.NoteHolder;
 import com.techbearcave.notetaker.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
 public class ListTasksActivity extends Activity {
+	
+	private ListView tasksListView;
+	private int editingNoteId = -1; 
+	private Cursor model;
+	private SQLiteHelper helper;
+	private NoteAdapter adapter;
+	private String userId;
+	public static String ID_EXTRA = "";
+	public static String ID_EDIT = "";
+	public static String ID_NOTE = "";
+	public static String isInEditMode;
+	
+	// Write the code here to delete the note from long press
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		tasksListView = (ListView)findViewById(R.id.tasksListView);
+		tasksListView.setAdapter(adapter);
+
+		long test = adapter.getItemId(info.position);
+		System.out.println("Here is the note position in the listview: " + test);
+		String taskId = String.valueOf(test);
+
+		Cursor c = helper.getNoteByNoteId(taskId, userId);
+		c.moveToFirst();
+		helper.deleteNote(taskId);
+
+		c.close();
+
+		tasksListView.setAdapter(adapter);
+		adapter.notifyDataSetChanged(); //this is suppose to notify adapter of changes and redraw listview
+
+
+		// 1. Find the position in ListView
+		// 2. Find NoteID in Listview
+		// 3. Send delete Query to database
+		// 4. Redisplay the Listview
+
+
+
+		return true;
+	}
+
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+
+		super.onCreateContextMenu(menu, v, menuInfo);
+
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.context_menu, menu);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list_tasks);
+
+		tasksListView = (ListView)findViewById(R.id.notesListView);
+		helper = new SQLiteHelper(this);
+		userId = getIntent().getStringExtra(NavigationMenu.ID_EXTRA);
+		
+		//generate context menu for long press - "Delete" note
+		registerForContextMenu(tasksListView);
+
+		model = helper.getNotesById(userId);
+		startManagingCursor(model);
+		adapter = new NoteAdapter(model);
+		tasksListView.setAdapter(adapter);
+		
+		tasksListView.setOnItemClickListener(onListClick);
 	}
 
 	@Override
@@ -20,5 +104,69 @@ public class ListTasksActivity extends Activity {
 		getMenuInflater().inflate(R.menu.list_tasks, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
 
+		
+		Intent editTaskIntent = new Intent(this, EditTaskActivity.class);
+		Bundle bundle = new Bundle();
+		bundle.putSerializable("stringToPassOn", userId);
+		
+		editTaskIntent.putExtras(bundle);
+		editTaskIntent.putExtra(isInEditMode, false);
+		startActivity(editTaskIntent);
+		
+		return true; 
+	}
+
+	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+			String hi = helper.getTaskId(model);
+			
+			Intent editTaskIntent = new Intent(view.getContext(), EditTaskActivity.class);
+			Bundle bundle = new Bundle ();
+			bundle.putSerializable("stringToPassOn", userId);
+			bundle.putSerializable("positionTracker", hi);
+			editTaskIntent.putExtras(bundle);
+			editTaskIntent.putExtra(isInEditMode, true);
+			startActivity(editTaskIntent);
+		}
+	};
+	
+	class NoteAdapter extends CursorAdapter {
+		NoteAdapter(Cursor c) {
+			super(ListTasksActivity.this, c);
+		}
+		
+		@Override
+		public void bindView(View row, Context ctxt, Cursor c) {
+			TaskHolder holder = (TaskHolder)row.getTag();
+			holder.populateFrom(c, helper);
+		}
+		
+		@Override
+		public View newView(Context ctxt, Cursor c, ViewGroup parent) {
+			LayoutInflater inflater = getLayoutInflater();
+			
+			View row = inflater.inflate(R.layout.row, parent, false);
+			TaskHolder holder = new TaskHolder(row);
+			row.setTag(holder);
+			return (row);
+		}
+	}
+	
+	class TaskHolder {
+		private TextView taskName = null;
+		private View row = null;
+		
+		TaskHolder(View row) {
+			this.row = row;
+			taskName = (TextView)row.findViewById(R.id.notename);
+		}
+		
+		void populateFrom(Cursor c, SQLiteHelper helper) {
+			taskName.setText(helper.getNotename(c));
+		}
+	}
 }

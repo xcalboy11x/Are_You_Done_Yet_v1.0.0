6497b58ab4785cccffabfb16b783add8a2301b6f
TimmyHuang <timmyhuang@outlook.com>
2014-04-10 22:06:01 -0400

    Fixed Navigation Menu layout + EditTask Design
    1. Fixed / Made buttons equal for Navigation Menu, looks a bit better /
    resized calendar. The buttons are bigger but I think it looks a bit
    better.
    2. EditTask design is done.

--- src/com/techbearcave/AYDY/EditTaskActivity.java
+++ src/com/techbearcave/AYDY/EditTaskActivity.java
@@ -10,6 +10,7 @@ import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -21,42 +22,94 @@ import com.techbearcave.notetaker.R;
 public class EditTaskActivity extends Activity implements OnItemSelectedListener {
 	
 	private CheckBox alertBox;
-	private Spinner monthSpinner;
+	private Spinner month31Spinner;
 	private Spinner daySpinner;
+	private Spinner hourSpinner;
+	private Spinner minuteSpinner;
+	private Spinner periodSpinner;
+	private Button setAlertButton;
+	//private boolean checkAlertBox;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_task);
 		
+		// set ID's for spinner & checkbox
 		alertBox = (CheckBox)findViewById(R.id.alertBox);
-		monthSpinner = (Spinner)findViewById(R.id.monthSpinner);
+		month31Spinner = (Spinner)findViewById(R.id.monthSpinner);
 		daySpinner =(Spinner)findViewById(R.id.daySpinner);
+		hourSpinner =(Spinner)findViewById(R.id.hourSpinner);
+		minuteSpinner =(Spinner)findViewById(R.id.minuteSpinner);
+		periodSpinner =(Spinner)findViewById(R.id.periodSpinner);
+		setAlertButton = (Button)findViewById(R.id.setAlertButton);
 		
-		ArrayAdapter monthAdapter = ArrayAdapter.createFromResource(this, R.array.month_array,android.R.layout.simple_spinner_item );
-		monthSpinner.setAdapter(monthAdapter);
-		monthSpinner.setOnItemSelectedListener(this);
-		monthSpinner.setVisibility(View.GONE);
+		//set the display adapters for spinners & button
+		ArrayAdapter monthAdapter = ArrayAdapter.createFromResource(this, R.array.month_31_array,android.R.layout.simple_spinner_item );
+		month31Spinner.setAdapter(monthAdapter);
+		month31Spinner.setOnItemSelectedListener(this);
+		month31Spinner.setVisibility(View.GONE);
 		
 		ArrayAdapter dayAdapter = ArrayAdapter.createFromResource(this, R.array.day_array,android.R.layout.simple_spinner_item );
 		daySpinner.setAdapter(dayAdapter);
 		daySpinner.setOnItemSelectedListener(this);
 		daySpinner.setVisibility(View.GONE);
 		
+		ArrayAdapter hourAdapter = ArrayAdapter.createFromResource(this, R.array.hour_array,android.R.layout.simple_spinner_item );
+		hourSpinner.setAdapter(hourAdapter);
+		hourSpinner.setOnItemSelectedListener(this);
+		hourSpinner.setVisibility(View.GONE);
+		
+		ArrayAdapter minuteAdapter = ArrayAdapter.createFromResource(this, R.array.minute_array,android.R.layout.simple_spinner_item );
+		minuteSpinner.setAdapter(minuteAdapter);
+		minuteSpinner.setOnItemSelectedListener(this);
+		minuteSpinner.setVisibility(View.GONE);
+		
+		ArrayAdapter periodAdapter = ArrayAdapter.createFromResource(this, R.array.period_array,android.R.layout.simple_spinner_item );
+		periodSpinner.setAdapter(periodAdapter);
+		periodSpinner.setOnItemSelectedListener(this);
+		periodSpinner.setVisibility(View.GONE);
+		
+		setAlertButton.setVisibility(View.GONE);
+		
+		//When notification checkbox is clicked due the following code - set spinners to view for data input
 		alertBox.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				
-				monthSpinner.setVisibility(View.VISIBLE);
+				month31Spinner.setVisibility(View.VISIBLE);
 				daySpinner.setVisibility(View.VISIBLE);
-				
+				hourSpinner.setVisibility(View.VISIBLE);
+				minuteSpinner.setVisibility(View.VISIBLE);
+				periodSpinner.setVisibility(View.VISIBLE);
+				setAlertButton.setVisibility(View.VISIBLE);
 				
 			}
 		});
 		
+		setAlertButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// send alert data to db
+				
+				month31Spinner.setVisibility(View.GONE);
+				daySpinner.setVisibility(View.GONE);
+				hourSpinner.setVisibility(View.GONE);
+				minuteSpinner.setVisibility(View.GONE);
+				periodSpinner.setVisibility(View.GONE);
+				setAlertButton.setVisibility(View.GONE);
+				alertBox.setChecked(false);
+				
+			}
+		});
+		
+		
 		}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		
@@ -65,20 +118,45 @@ public class EditTaskActivity extends Activity implements OnItemSelectedListener
       
 	}
 
+	//when the data is selected per spinner do this:
 	@Override
 	public void onItemSelected(AdapterView<?> arg0, View view, int arg2,
 			long arg3) {
 	
-		if(monthSpinner.getId() == R.id.monthSpinner)
+		if(month31Spinner.getId() == R.id.monthSpinner)
 		{
 		
 		TextView textFieldSelected = (TextView) view ;
 		Toast.makeText(this, "You Selected " + textFieldSelected.getText(), Toast.LENGTH_LONG).show();
-				
+			
 			
 		}
 		
 		if(daySpinner.getId() == R.id.daySpinner)
+		{
+		
+		TextView textFieldSelected = (TextView) view ;
+		Toast.makeText(this, "You Selected " + textFieldSelected.getText(), Toast.LENGTH_LONG).show();
+		
+		}
+		
+		if(hourSpinner.getId() == R.id.hourSpinner)
+		{
+		
+		TextView textFieldSelected = (TextView) view ;
+		Toast.makeText(this, "You Selected " + textFieldSelected.getText(), Toast.LENGTH_LONG).show();
+		
+		}
+		
+		if(minuteSpinner.getId() == R.id.minuteSpinner)
+		{
+		
+		TextView textFieldSelected = (TextView) view ;
+		Toast.makeText(this, "You Selected " + textFieldSelected.getText(), Toast.LENGTH_LONG).show();
+		
+		}
+		
+		if(periodSpinner.getId() == R.id.periodSpinner)
 		{
 		
 		TextView textFieldSelected = (TextView) view ;
@@ -91,6 +169,6 @@ public class EditTaskActivity extends Activity implements OnItemSelectedListener
 		
 		
 	}
-
+	
 
 }
